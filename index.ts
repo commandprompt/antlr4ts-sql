66ea54c6c3ee07f2ac11c80150a5165ac67092c5
tatsuya.atsumi <t.atsumi@dr-ubie.com>
2021-11-14 15:41:48 +0900

    export bq

--- index.ts
+++ index.ts
@@ -1,29 +1,33 @@
-export * from 'antlr4ts';
-export * from 'antlr4ts/atn';
-export * from 'antlr4ts/dfa';
-export * from 'antlr4ts/misc';
-export * from 'antlr4ts/tree';
-export * from 'antlr4ts/tree/pattern';
-export * from 'antlr4ts/tree/xpath';
+export * from "antlr4ts";
+export * from "antlr4ts/atn";
+export * from "antlr4ts/dfa";
+export * from "antlr4ts/misc";
+export * from "antlr4ts/tree";
+export * from "antlr4ts/tree/pattern";
+export * from "antlr4ts/tree/xpath";
 
-export * from './src/antlr4tsSQL';
+export * from "./src/antlr4tsSQL";
 
-export * from './src/models/SQLDialect';
-export * from './src/models/CaseChangingStream';
+export * from "./src/models/SQLDialect";
+export * from "./src/models/CaseChangingStream";
 
-export * as MySQLGrammar from './src/grammar-output/mysql/MultiQueryMySQLParser';
-export * from './src/grammar-output/mysql/MultiQueryMySQLParserListener';
-export * from './src/grammar-output/mysql/MySQLLexer';
-export * from './src/grammar-output/mysql/common';
+export * as MySQLGrammar from "./src/grammar-output/mysql/MultiQueryMySQLParser";
+export * from "./src/grammar-output/mysql/MultiQueryMySQLParserListener";
+export * from "./src/grammar-output/mysql/MySQLLexer";
+export * from "./src/grammar-output/mysql/common";
 
-export * as PLpgSQLGrammar from './src/grammar-output/plpgsql/PLpgSQLParser';
-export * from './src/grammar-output/plpgsql/PLpgSQLParserListener';
-export * from './src/grammar-output/plpgsql/PLpgSQLLexer';
+export * as PLpgSQLGrammar from "./src/grammar-output/plpgsql/PLpgSQLParser";
+export * from "./src/grammar-output/plpgsql/PLpgSQLParserListener";
+export * from "./src/grammar-output/plpgsql/PLpgSQLLexer";
 
-export * as PlSQLGrammar from './src/grammar-output/plsql/PlSqlParser';
-export * from './src/grammar-output/plsql/PlSqlParserListener';
-export * from './src/grammar-output/plsql/PlSqlLexer';
+export * as PlSQLGrammar from "./src/grammar-output/plsql/PlSqlParser";
+export * from "./src/grammar-output/plsql/PlSqlParserListener";
+export * from "./src/grammar-output/plsql/PlSqlLexer";
 
-export * as TSQLGrammar from './src/grammar-output/tsql/TSqlParser';
-export * from './src/grammar-output/tsql/TSqlParserListener';
-export * from './src/grammar-output/tsql/TSqlLexer';
+export * as TSQLGrammar from "./src/grammar-output/tsql/TSqlParser";
+export * from "./src/grammar-output/tsql/TSqlParserListener";
+export * from "./src/grammar-output/tsql/TSqlLexer";
+
+export * as BigQueryGrammar from "./src/grammar-output/bigquery/BigQueryParser";
+export * from "./src/grammar-output/bigquery/BigQueryParserListener";
+export * from "./src/grammar-output/bigquery/BigQueryLexer";
