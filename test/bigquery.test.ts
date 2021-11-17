49a0ccc83ef0c2e7334f2ff513c73b9b4d44b08c
tatsuya.atsumi <t.atsumi@dr-ubie.com>
2021-11-17 23:12:46 +0900

    revert

--- test/bigquery.test.ts
+++ test/bigquery.test.ts
@@ -1,4 +1,4 @@
-import { antlr4tsSQL, SQLDialect } from "../index";
+import { antlr4tsSQL, SQLDialect, PredictionMode } from "../index";
 import { BigQueryGrammar } from "../index";
 
 let antlr4tssql = null;
@@ -7,11 +7,14 @@ beforeAll(() => {
 });
 
 test("can parse and tokenize a query", () => {
-  const query = "SELECT * FROM `sch1.table";
-  const tokens = antlr4tssql.getTokens(query);
-  const parser = antlr4tssql.getParser(tokens);
+  const query = `SELECT
+  *
+FROM \`sch1.table`;
+  const tokens = antlr4tssql.getTokens(query, []);
+  const parser = antlr4tssql.getParser(tokens, []);
+  parser.interpreter.setPredictionMode(PredictionMode.LL);
   expect(parser instanceof BigQueryGrammar.BigQueryParser).toBeTruthy();
   const parseTree = antlr4tssql.getParseTree(parser);
-  expect(tokens.getTokens().length).toBe(7 + 1); // 4 "words", 1 EOF, the BigQuery Grammar skips whitespace characters
+  expect(tokens.getTokens().length).toBe(7 + 1); // 7 "words", 1 EOF, the BigQuery Grammar skips whitespace characters
   expect(parseTree instanceof BigQueryGrammar.SqlContext).toBeTruthy();
 });
